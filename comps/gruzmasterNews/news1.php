db4cb1b7be1fdb97dd6e1aa0210f237aabee239f
Novikov <942003@bk.ru>
2023-02-16 15:39:16 +0300

    add_require

--- comps/gruzmasterNews/news1.php
+++ comps/gruzmasterNews/news1.php
@@ -15,36 +15,9 @@
 	<meta name="viewport" content="width=device-width">
 </head>
 <body>
-		<header id="header" class="header">		
-				<div class="flex__row">
-						<a href="../../main"><img src="../../img/LogoWhite.svg" ></a>
-						<p class="header__p"><a class="a__hover" href="../../EpkAskella">ЕПК АСКЕЛЛА</a></p>
-						<p class="header__p"><a class="a__hover" href="../../companies">Компании</a></p>
-						<p class="header__p"><a class="a__hover" href="../../improve">Устойчивое развитие</a></p>
-						<p class="header__p"><a class="a__hover modalOpener" href="#hotline">Горячая линия</a></p>
-						<a href="#contactUs"><div class="butt header__text3 modalOpener"><p  class="header__p2">СВЯЗАТЬСЯ С НАМИ</p></div></a>
-						<div class="header__last">
-							<p class="lang__p"><a data-google-lang="en"  class="a__hover"  style="cursor: pointer;">En</a></p>
-							<a class="modalOpener" href="#menu"><img  src="../../img/footWhite.png" class="header__foot__head"></a>
-						</div>
-				</div>
-			
-		</header>
-		<!--  -->
-		<div class="menuTop">	<!-- menuTop: opacity 1 -->
-			<div class="flex__row">
-					<a href="../../main"><img src="../../img/LogoWhite.svg" class="LogoImg"></a>
-					<p class="header__p"><a class="a__hover" href="../../EpkAskella">ЕПК АСКЕЛЛА</a></p>
-					<p class="header__p"><a class="a__hover" href="../../companies">Компании</a></p>
-					<p class="header__p"><a class="a__hover" href="../../improve">Устойчивое развитие</a></p>
-					<p class="header__p"><a class="a__hover modalOpener" href="#hotline">Горячая линия</a></p>
-					<a  href="#contactUs"><div class="butt header__text3 modalOpener"><p class="header__p2">СВЯЗАТЬСЯ С НАМИ</p></div></a>
-					<div class="header__last">
-						<p class="lang__p"><a data-google-lang="en"  class="a__hover"  style="cursor: pointer;">En</a></p>
-						<a class="modalOpener" href="#menu"><img  src="../../img/footWhite.png" class="header__foot"></a>
-					</div>
-			</div>
-		</div>
+		<?php
+			require  dirname(dirname(__DIR__)). DIRECTORY_SEPARATOR . 'headerWhite.php';
+		?>
 
 		<div class="wrapper">
 			<div class="content">
